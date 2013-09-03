ac130ee28ecd720b46f8ddf16b2cf0d4dee4cb1a
ggomagundan <pbs52@hanmail.net>
2013-09-03 13:30:28 +0900

    modify bugs

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,6 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn_error.log"
 stdout_path "#{root}/log/unicorn_access.log"
 
-listen "#{root}/tmp/unicorn.sock"
+listen "/tmp/unicorn.sock"
 worker_processes 2
 timeout 30
