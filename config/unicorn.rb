e0e657659ed3134c8093ffa2c26724e9198263eb
ggomagundan <pbs52@hanmail.net>
2013-09-09 02:20:58 +0900

    modify bugs

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,9 @@
 root = "/var/www/todpop/web/current"
+working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn_error.log"
 stdout_path "#{root}/log/unicorn_access.log"
 
 listen "/tmp/unicorn.sock"
-worker_processes 2
+worker_processes 4
 timeout 30
