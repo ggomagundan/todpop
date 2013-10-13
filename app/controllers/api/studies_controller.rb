935cd4319dc9cd47ba7e0fe2253ca8cc600f9be3
JSKim <ddubool92@gmail.com>
2013-10-14 05:12:30 +0900

    send word add logger

--- app/controllers/api/studies_controller.rb
+++ app/controllers/api/studies_controller.rb
@@ -131,7 +131,7 @@ class Api::StudiesController < ApplicationController
     def send_word_result
       @status = true
       @msg = ""
-      puts "1111111111111"
+      Rails.logger.debug "1111111111111"
       if !params[:level].present? || !params[:stage].present?
         @status = false
         @msg = "not exist level or stage parameter"   
@@ -148,7 +148,7 @@ class Api::StudiesController < ApplicationController
         @status = false
         @msg = "not exist category parameter"   
       end
-      puts "2222222222222"
+      Rails.logger.debug "2222222222222"
   if @status == true
       stage = params[:stage].to_i
       level = params[:level].to_i
@@ -162,7 +162,7 @@ class Api::StudiesController < ApplicationController
     
       chaining = 0
       chain_point =0
-      puts "3333333333333333333"
+      Rails.logger.debug "3333333333333333333"
       record = UserRecord.where(:stage => stage, :level => level, :id => user_id).first
      (1..result.length).each do |i|
        if result[i] == "2"
@@ -183,7 +183,7 @@ class Api::StudiesController < ApplicationController
 
         end
      end 
-      puts "44443"
+      Rails.logger.debug "44443"
     
       if stage >= 1 && stage < 10
         @score = (fast  + middle)  * 100 / exam_count
@@ -199,7 +199,7 @@ class Api::StudiesController < ApplicationController
 
         @rank_point = result / count * 20
         
-      puts "555555"
+      Rails.logger.debug "555555"
       else 
         @status = false
         @msg = "stage must 1~10"
@@ -213,7 +213,7 @@ class Api::StudiesController < ApplicationController
         else 
             @medal = 0
         end
-puts "666666"
+Rails.logger.debug "666666"
          if record.present? && record.record_point.present?
            @rank_point = @rank_point / 2
            
@@ -231,7 +231,7 @@ puts "666666"
              @reward  = 0
            end
          end
-puts "77777777"
+Rails.logger.debug "77777777"
 
          if record.present?
            if record.record_type < @medal
@@ -258,7 +258,7 @@ puts "77777777"
            end
 
          end
-puts "88888888"
+Rails.logger.debug "88888888"
           if record.present? &&  @medal < record.record_type
             @medal = record.record_type
           end
