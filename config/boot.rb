6cd32fe9644a825caabade956bf41447408314cc
honeyl3oom <ddubool92@gmail.com>
2013-10-25 05:13:28 +0900

    modify gem path

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,4 @@
 # -*- encoding : utf-8 -*-
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-ENV['GEM_PATH'] ||= "/todpop/shared/bundle/ruby/2.0.0/gems"
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
