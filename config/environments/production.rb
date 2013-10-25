f3ba95ca5af40f94b17ca4c42845647df1887487
honeyl3oom <ddubool92@gmail.com>
2013-10-25 09:09:43 +0900

    add gem path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 # -*- encoding : utf-8 -*-
+ENV['GEM_PATH'] = "/todpop/todpop_app/shared/bundle/ruby/2.0.0"
 Todpop::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -34,14 +35,13 @@ Todpop::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
-  config.assets.debug = true
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
