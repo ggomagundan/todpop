89c4581a070ac50ac68b4e55fa3f7260ab25e8a3
honeyl3oom <ddubool92@gmail.com>
2013-10-28 05:08:10 +0900

    cps survey content update bug fixed & possible to input enter key..

--- app/models/survey_content.rb
+++ app/models/survey_content.rb
@@ -3,10 +3,11 @@ class SurveyContent < ActiveRecord::Base
 	belongs_to :cpx_advertisement
 	mount_uploader :q_image, ImageUploader
 	Q_TYPE={
-     :NO_IMAGE => 1,
-     :IMAGE => 2,
-     :NO_IMAGE_SINGLE => 3,
-     :IMAGE_SINGLE => 4,
+    :CPS_SPEC => 0,
+    :NO_IMAGE_CHOICE_ANSWER => 1,
+    :IMAGE_CHOICE_ANSWER => 2,
+    :NO_IMAGE_SHORT_ANSWER => 3,
+    :IMAGE_SHORT_ANSWER => 4
   }
   def kind
    return SurveyContent::Q_TYPE.key(self.q_type)
