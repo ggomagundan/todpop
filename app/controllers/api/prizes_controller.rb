a2fc0126927628ff86abc18057fbbc8bbd07de71
JSKim <ddubool92@gmail.com>
2013-10-30 00:09:22 +0900

    cps create bug fix & remove not needs..

--- app/controllers/api/prizes_controller.rb
+++ app/controllers/api/prizes_controller.rb
@@ -1,13 +1,5 @@
 # -*- encoding : utf-8 -*-
 class Api::PrizesController < ApplicationController
-  def index
-    @prizes = Prize.all
-  end
-
-  def show
-    @prize = Prize.find(params[:id])
-  end
-
 
   def get_prize_info
     @status = true
@@ -23,36 +15,5 @@ class Api::PrizesController < ApplicationController
     end
 
   end
-
-  def new
-    @prize = Prize.new
-  end
-
-  def create
-    @prize = Prize.new(params[:prize])
-    if @prize.save
-      redirect_to [:api, @prize], :notice => "Successfully created prize."
-    else
-      render :action => 'new'
-    end
-  end
-
-  def edit
-    @prize = Prize.find(params[:id])
-  end
-
-  def update
-    @prize = Prize.find(params[:id])
-    if @prize.update_attributes(params[:prize])
-      redirect_to [:api, @prize], :notice  => "Successfully updated prize."
-    else
-      render :action => 'edit'
-    end
-  end
-
-  def destroy
-    @prize = Prize.find(params[:id])
-    @prize.destroy
-    redirect_to api_prizes_url, :notice => "Successfully destroyed prize."
-  end
+  
 end
