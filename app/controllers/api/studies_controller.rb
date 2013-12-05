2de6788ba60516012d630f37121e0a60ff67c915
jinooaction <5jinoo@naver.com>
2013-12-05 15:17:25 +0900

    send_word_result_error

--- app/controllers/api/studies_controller.rb
+++ app/controllers/api/studies_controller.rb
@@ -443,7 +443,17 @@ class Api::StudiesController < ApplicationController
       UserTestHistory.create(:user_id => user_id, :category => category, :level => level, :stage => stage, :n_medals => @medal, :score => @score, :reward => @reward, :rank_point => @rank_point)
 
       # INPUT => User_Stage_Info
-      usinfo = UserStageInfo.find_by_user_id(user_id)
+      if !UserStageInfo.find_by_user_id(user_id).present?
+        cusinfo = UserStageInfo.new
+        cusinfo.user_id = user_id
+        cusinfo.stage_info = "0"
+        (1..1799).each do
+          cusinfo.stage_info += "X"
+        end
+        cusinfo.save
+      else
+        usinfo = UserStageInfo.find_by_user_id(user_id)
+      end
       if usinfo.stage_info[(level-1)*10 + (stage-1)].to_i < @medal
         usinfo.stage_info[(level-1)*10 + (stage-1)] = @medal.to_s
       end
