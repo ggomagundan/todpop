a3571991e7eb3e325aa17c5d45dccb4a779a5b2a
ggomagundan <ggogun@gmail.com>
2014-01-19 15:34:06 +0900

    modify bugs

--- app/controllers/api/qpcon_coupons_controller.rb
+++ app/controllers/api/qpcon_coupons_controller.rb
@@ -5,9 +5,9 @@ class Api::QpconCouponsController < ApplicationController
     @msg =""
   
     @coupons = QpconProduct.all
-  if params[:category_id].present?
-    @coupons = @coupons.where(:qpcon_category_id => params[:category_id])
-  end
+    if params[:category_id].present?
+      @coupons = @coupons.where(:qpcon_category_id => params[:category_id])
+    end
 
 
   end
@@ -53,12 +53,12 @@ class Api::QpconCouponsController < ApplicationController
       user.update_attributes(:total_reward => user.total_reward - coupon.common_cost)
 
       last_uri = "pinIssue.do"
-      c_params = {:prodId => coupon.product_id,:reqOrdId => Time.now.to_datetime.strftime('%Y-%m-%d %H:%M:%S.%N') }
+      c_params = {:prodId => coupon.product_id,:reqOrdId => Time.now.to_datetime.strftime('%Y-%m-%d %H:%M:%S.%N'), :key => "0f8f5a7024dd11e3b5ae00304860c864"}
 
       uri = URI.parse("http://211.245.169.201/qpcon/api/pin/#{last_uri}")
       http = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Post.new(uri.request_uri)
-      request.set_form_data(c_params.merge!({:key=> KEY}))
+      request.set_form_data(c_params)
       @response = http.request(request)
       @response = @response.body
 
