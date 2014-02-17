a9da63e533e613e220f1924107480d22cf1270ba
cystein <ever4cys@gmail.com>
2014-02-17 19:50:13 +0900

    cys : qpcon purchase API almost DONE

--- app/controllers/api/qpcon_coupons_controller.rb
+++ app/controllers/api/qpcon_coupons_controller.rb
@@ -22,7 +22,7 @@ class Api::QpconCouponsController < ApplicationController
     if !user.is_set_facebook_password
       @status = false
       @result = false
-      @msg ="상품구매와 장학금 수여를 위해서는 비밀번호를 설정해야 합니다. 마이페이지에서 비밀번호를 설정해주세요"
+      @msg ="need to set password"
     end
   end
 
@@ -34,107 +34,250 @@ class Api::QpconCouponsController < ApplicationController
   end 
 
 
+  # --------------------------------------------------------------------------------------------------------------------
   def purchase
     @status = true
     @msg = ""
     @result = false
-    @return_msg = ""
+    @return_msg = "purchase start"
 
     user =  User.find_by_id(params[:user_id])
     coupon = QpconProduct.find_by_product_id(params[:product_id])
 
-    if !user.authenticate(params[:password]).present?
+    if !params[:user_id].present? || !params[:password].present? || !params[:product_id].present?
       @status = false
-      @msg = "비밀번호가 잘못 되었습니다."
+      @msg = "need params"
+    elsif !user.present?
+      @status = false
+      @msg = "wrong user_id"
+    elsif !user.authenticate(params[:password]).present?
+      @status = false
+      @msg = "wrong password"
+    elsif !coupon.present?
+      @status = false
+      @msg = "no coupon found (by product id)"
     end
 
     if @status == true
+      if user.current_reward >= coupon.market_cost
 
-      if user.current_reward >= coupon.common_cost
+        qpcon = set_qpcon(user,coupon)
+        qpcon = issue_qpcon(qpcon)
 
-        timenow = Time.now.to_datetime 
-        server_uri = "http://211.245.169.201/qpcon/api/pin"        # qpcon_test_server_URI
-        
-        qpcon_key       = "0f8f5a7024dd11e3b5ae00304860c864"       # qpcon_test_server_key_for_todpop
-        qpcon_reqOrdId  = user.id.to_s + '_' + timenow.strftime('%Y%m%d') + '_' + timenow.strftime('%H%M%S') + '_' + timenow.strftime('%N')
-        qpcon_prodId    = coupon.product_id
-        qpcon_pinCnt    = 1
-        qpcon_payGb     = nil
-        qpcon_reserved1 = nil
-        qpcon_reserved2 = nil
-
-        issue_uri = URI.parse(server_uri + "/pinIssue.do")
-        issue_params = {:key => qpcon_key, :reqOrdId => qpcon_reqOrdId, :prodId => qpcon_prodId}
-
-        http = Net::HTTP.new(issue_uri.host, issue_uri.port)
-        request = Net::HTTP::Post.new(issue_uri.request_uri)
-        request.set_form_data(issue_params)
-        @response = http.request(request)
-        @response = @response.body
-
-        response_all = @response.split("|")
-        response_code     = response_all[0]
-        response_msg      = response_all[1]
-        response_reqOrdId = response_all[2]
-        response_pin      = response_all[3]
-        response_valid    = response_all[4]
-        response_approval = response_all[5]
-        response_date     = response_all[6]
-
-        @return_msg = response_msg
-
-        if response_code == "00"
-
-          qpcon_admitId = response_approval
-        
-          admit_uri = URI.parse(server_uri + "/pinIssueConfirm.do")
-          admit_params = {:key => qpcon_key, :admitId => qpcon_admitId}
-
-          http = Net::HTTP.new(admit_uri.host, admit_uri.port)
-          request = Net::HTTP::Post.new(admit_uri.request_uri)
-          request.set_form_data(admit_params)
-          @response2 = http.request(request)
-          @response2 = @response2.body
-
-          response2_all = @response2.split("|")
-          response2_code  = response2_all[0]
-          response2_msg   = response2_all[1]
-
-          @return_msg = response_msg + '_' + response2_msg
-
-          if response2_code == "00"
-
-            @result=true
-
-            Order.create(:coupon_company => "qpcon", :user_id => user.id, :order_id => response_reqOrdId, :product_id => coupon.product_id, :barcode => response_pin, :limit_date => Date.strptime(response_valid,"%Y%m%d"), :approval_number => response_approval, :issue_date => response_date)
-
-            #user.update_attributes(:total_reward => user.total_reward - coupon.common_cost)
-            # reward process
-            @token_user_id = user.id
-            @token_reward_type = 7000
-            @token_title = "상품구매:큐피콘"
-            @token_sub_title = QpconProduct.find_by_id(coupon.id).product_name
-            @token_reward = (-1).to_i * coupon.common_cost
-            process_reward_general
-            # ---------
-
-          else
-            @result = false
-            @msg = "issue OK, admit FAIL"
+        if qpcon[:respCode] == "10"     # conditional issue OK check
+          qpcon = issue_query_qpcon(qpcon)
+          if qpcon[:respCode] != "00"
+            sleep(1)                           # sleep 1 sec
+            qpcon = issue_query_qpcon(qpcon)   # retry only one more time
           end
-       
-        else
-          @result = false
-          @msg = "issue FAIL"
         end
-    
-      else
-        @msg = "장학금이 부족합니다"
-        @result = false
-        @status = false
-      end
 
-    end
+        if qpcon[:respCode] == "00"     # 2 cases : direct OK or conditional-then-final OK
+          qpcon = confirm_qpcon(qpcon)
+          if qpcon[:respCode] == "00"     # confirm OK
+            save_log_qpcon(user,coupon,qpcon)
+            @result=true
+          else                             # confirm Fail
+            qpcon = cancel_qpcon(qpcon)    # suggested by qpcon guide
+
+            cnt=3
+            while qpcon[:respCode]!="00" && cnt>0
+              sleep(0.5)                         # sleep 0.5 sec
+              qpcon = cancel_qpcon(qpcon)        # retry 3 more time (total 4 times : 4 = trivial)
+              cnt = cnt-1 
+            end
+          end  # end of confirm branch (00 or else)
+        end    # end of issue OK
+
+      else
+        @status = false 
+        @msg = "not enouth money"
+        @result = false
+      end      # end of enough money
+    end        # end of status:true
+
+  end          # end of def
+
+  # --------------------------------------------------------------------------------------------------------------------
+  def set_qpcon(user,coupon)
+
+    qpcon = {}
+    timenow = Time.now.to_datetime 
+    qpcon[:serverUri] = "http://211.245.169.201/qpcon/api/pin"        # qpcon_test_server_URI
+    #qpcon[:serverUri] = "http://211.233.60.195/qpcon/api/pin"        # qpcon_operation_server_URI
+        
+    qpcon[:key]        = "0f8f5a7024dd11e3b5ae00304860c864"       # qpcon_test_server_key_for_todpop
+    qpcon[:reqOrdId]  = user.id.to_s + '_' + timenow.strftime('%Y%m%d') + '_' + timenow.strftime('%H%M%S') + '_' + timenow.strftime('%N')
+    qpcon[:prodId]    = coupon.product_id
+    qpcon[:pinCnt]    = 1
+    qpcon[:payGb]     = nil
+    qpcon[:reserved1] = nil
+    qpcon[:reserved2] = nil
+
+    return qpcon
+  end
+  # --------------------------------------------------------------------------------------------------------------------
+
+  def issue_qpcon(qpcon)
+
+    issue_uri = URI.parse(qpcon[:serverUri] + "/pinIssue.do")
+    issue_params = {:key => qpcon[:key], :reqOrdId => qpcon[:reqOrdId], :prodId => qpcon[:prodId]}
+
+    # clear corresponding resp
+    qpcon = qpcon.merge(:respCode => nil, :respMsg => nil, :reqOrdId => nil, :pinNum => nil, :validDate => nil, :admitId => nil, :issueDate => nil)
+
+    http = Net::HTTP.new(issue_uri.host, issue_uri.port)
+    request = Net::HTTP::Post.new(issue_uri.request_uri)
+    request.set_form_data(issue_params)
+    response_all = http.request(request)
+    response = response_all.body
+
+    response_split = response.split("|")
+    qpcon[:respCode]   = response_split[0]
+    qpcon[:respMsg]    = response_split[1]
+    qpcon[:reqOrdId]   = response_split[2]
+    qpcon[:pinNum]     = response_split[3]
+    qpcon[:validDate]  = response_split[4]
+    qpcon[:admitId]    = response_split[5]
+    qpcon[:issueDate]  = response_split[6]
+
+    @return_msg = @return_msg.present? ? @return_msg + ' | ' + qpcon[:respMsg] : qpcon[:respMsg]
+
+    return qpcon
 
   end
+  # --------------------------------------------------------------------------------------------------------------------
+
+  def cancel_qpcon(qpcon)
+
+    cancel_uri = URI.parse(qpcon[:serverUri] + "/pinCancel.do")
+    cancel_params = {:key => qpcon[:key], :admitId => qpcon[:admitId]}
+
+    # clear corresponding resp
+    qpcon = qpcon.merge(:respCode => nil, :respMsg => nil, :cancelDate => nil)
+
+    http = Net::HTTP.new(cancel_uri.host, cancel_uri.port)
+    request = Net::HTTP::Post.new(cancel_uri.request_uri)
+    request.set_form_data(cancel_params)
+    response_all = http.request(request)
+    response = response_all.body
+
+    response_split = response.split("|")
+    qpcon[:respCode]    = response_split[0]
+    qpcon[:respMsg]     = response_split[1]
+    qpcon[:cancelDate]  = response_split[2]
+
+    @return_msg = @return_msg.present? ? @return_msg + ' | ' + qpcon[:respMsg] : qpcon[:respMsg]
+
+    return qpcon
+
+  end
+  # --------------------------------------------------------------------------------------------------------------------
+
+  def info_qpcon(qpcon)
+
+    info_uri = URI.parse(qpcon[:serverUri] + "/pinInfo.do")
+    info_params = {:key => qpcon[:key], :admitId => qpcon[:admitId], :pinNum => qpcon[:pinNum]}
+
+    # clear corresponding resp
+    qpcon = qpcon.merge(:respCode => nil, :respMsg => nil, :reqOrdId => nil, :issueDate => nil, :admitId => nil, :pinStatus => nil, :useDate => nil, :cancelDate => nil, :validDate => nil)
+
+    http = Net::HTTP.new(info_uri.host, info_uri.port)
+    request = Net::HTTP::Post.new(info_uri.request_uri)
+    request.set_form_data(info_params)
+    response_all = http.request(request)
+    response = response_all.body
+
+    response_split = response.split("|")
+    qpcon[:respCode]     = response_split[0]
+    qpcon[:respMsg]      = response_split[1]
+    qpcon[:reqOrdId]     = response_split[2]
+    qpcon[:issueDate]    = response_split[3]
+    qpcon[:admitId]      = response_split[4]
+    qpcon[:pinStatus]    = response_split[5]
+    qpcon[:useDate]      = response_split[6]
+    qpcon[:cancelDate]   = response_split[7]
+    qpcon[:validDate]    = response_split[8]
+
+    @return_msg = @return_msg.present? ? @return_msg + ' | ' + qpcon[:respMsg] : qpcon[:respMsg]
+
+    return qpcon
+
+  end
+  # --------------------------------------------------------------------------------------------------------------------
+
+  def issue_query_qpcon(qpcon)
+
+    query_uri = URI.parse(qpcon[:serverUri] + "/pinIssueResult.do")
+    query_params = {:key => qpcon[:key], :reqOrdId => qpcon[:reqOrdId], :prodId => qpcon[:prodId]}
+
+    # clear corresponding resp
+    qpcon = qpcon.merge(:respCode => nil, :respMsg => nil, :reqOrdId => nil, :pinNum => nil, :validDate => nil, :admitId => nil, :issueDate => nil)
+
+    http = Net::HTTP.new(query_uri.host, query_uri.port)
+    request = Net::HTTP::Post.new(query_uri.request_uri)
+    request.set_form_data(query_params)
+    response_all = http.request(request)
+    response = response_all.body
+
+    response_split = response.split("|")
+    qpcon[:respCode]   = response_split[0]
+    qpcon[:respMsg]    = response_split[1]
+    qpcon[:reqOrdId]   = response_split[2]
+    qpcon[:pinNum]     = response_split[3]
+    qpcon[:validDate]  = response_split[4]
+    qpcon[:admitId]    = response_split[5]
+    qpcon[:issueDate]  = response_split[6]
+
+    @return_msg = @return_msg.present? ? @return_msg + ' | ' + qpcon[:respMsg] : qpcon[:respMsg]
+
+    return qpcon
+
+  end
+  # --------------------------------------------------------------------------------------------------------------------
+
+  def confirm_qpcon(qpcon)
+
+    confirm_uri = URI.parse(qpcon[:serverUri] + "/pinIssueConfirm.do")
+    confirm_params = {:key => qpcon[:key], :admitId => qpcon[:admitId]}
+
+    # clear corresponding resp
+    qpcon = qpcon.merge(:respCode => nil, :respMsg => nil)
+
+    http = Net::HTTP.new(confirm_uri.host, confirm_uri.port)
+    request = Net::HTTP::Post.new(confirm_uri.request_uri)
+    request.set_form_data(confirm_params)
+    response_all = http.request(request)
+    response = response_all.body
+
+    response_split = response.split("|")
+    qpcon[:respCode]  = response_split[0]
+    qpcon[:respMsg]   = response_split[1]
+
+    @return_msg = @return_msg.present? ? @return_msg + ' | ' + qpcon[:respMsg] : qpcon[:respMsg]
+
+    return qpcon
+
+  end
+  # --------------------------------------------------------------------------------------------------------------------
+
+  def save_log_qpcon(user,coupon,qpcon)
+
+    Order.create(:coupon_company => "qpcon", :user_id => user.id, :order_id => qpcon[:reqOrdId], :product_id => coupon.product_id, :barcode => qpcon[:pinNum], :limit_date => Date.strptime(qpcon[:validDate],"%Y%m%d"), :approval_number => qpcon[:admitId], :issue_date => qpcon[:issueDate])
+
+    # reward process
+    @token_user_id = user.id
+    @token_reward_type = 7000
+    @token_title = "상품구매:큐피콘"
+    @token_sub_title = QpconProduct.find_by_product_id(coupon.product_id).product_name
+    @token_reward = (-1).to_i * coupon.market_cost
+    process_reward_general
+    # ---------
+  end
+
+  # --------------------------------------------------------------------------------------------------------------------
+
+
+
+
 end
