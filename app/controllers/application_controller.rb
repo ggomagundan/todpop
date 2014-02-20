266b73713f60005732e99454049a67a052b9d5a5
jinooaction <5jinoo@naver.com>
2014-02-20 16:28:35 +0900

    session_log_in_err

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #
-  layout 'admin'
   protect_from_forgery with: :exception
 
 
