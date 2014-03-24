6dde7db984dce26465fefaf61ccdff466cebc75a
jinooaction <5jinoo@naver.com>
2014-03-24 12:40:01 +0900

    advertises_controller.rb

--- app/controllers/api/advertises_controller.rb
+++ app/controllers/api/advertises_controller.rb
@@ -438,13 +438,9 @@ class Api::AdvertisesController < ApplicationController#< Api::ApplicationContro
             r_id = CpxAdvertisement.where('ad_type = 300').pluck(:id).first
             r_id = 0 if r_id == nil
           else
-            r_id = 0
-            ad_tmp.each do |i|
-              a = AdvertiseCpxLog.where('user_id = ? and ad_id = ?', params[:user_id].to_i, i).count
-              if a > r_id
-                r_id = i
-              end
-            end
+            log = AdvertiseCpxLog.where('user_id =? and ad_id in (?)', params[:user_id].to_i , ad_tmp).pluck(:ad_id)
+            r_id = CpxAdvertisement.where('id > ? and ad_type not in (300)', log.last).first
+            r_id = log.min if !r_id.present?
           end
         end
 
