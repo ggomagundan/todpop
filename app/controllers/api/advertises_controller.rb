5366b1737084f42bfbeb7e977ba0d8c82c5c3892
jinooaction <5jinoo@naver.com>
2014-03-25 16:22:47 +0900

    advertises_controller.rb

--- app/controllers/api/advertises_controller.rb
+++ app/controllers/api/advertises_controller.rb
@@ -681,6 +681,7 @@ class Api::AdvertisesController < ApplicationController#< Api::ApplicationContro
         @status = false
         @msg = "not exist cpx or user"
       else
+          if !AdvertiseCpxLog.where('user_id = ? and ad_id = ? and act = 3', params[:user_id].to_i, params[:ad_id].to_i).present?
         adLog = AdvertiseCpxLog.new
         adLog.ad_id = params[:ad_id]
         adLog.ad_type = params[:ad_type]
@@ -689,7 +690,6 @@ class Api::AdvertisesController < ApplicationController#< Api::ApplicationContro
         if adLog.save
           @result = true
           @msg = "success"
-          if !AdvertiseCpxLog.where('user_id = ? and ad_id = ? and act = 3', params[:user_id].to_i, params[:ad_id].to_i).present?
           if params[:act].to_i==3
             adInfo.update_attributes(:remain => adInfo.remain - 1)
 
